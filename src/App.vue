ade4e345ea96b92fa3a59d4392cb489aec5a36e0
Choe Hyeonyong <gusdyddl1212@naver.com>
2024-07-31 17:51:11 +0900

    feat: kakaologin button, link

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,55 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <h1>SSAFY MANAGER</h1>
   </header>
-
-  <RouterView />
+  <button class="login-btn" @click="kakaoLogin()">
+    <img class="kakao-logo" src="./assets/kakao-talk.png" />카카오톡으로
+    시작하기
+  </button>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  methods: {
+    kakaoLogin: function () {
+      const REST_API_KEY = import.meta.env.VITE_APP_REST_API_KEY;
+      const REDIRECT_URI = encodeURIComponent(
+        import.meta.env.VITE_APP_REDIRECT_URI
+      );
+      const REDIRECT_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+      console.log('눌렀음');
+      window.location.href = REDIRECT_URL;
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+body {
+  display: grid;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.login-btn {
+  width: 200px;
+  display: flex;
+  border-radius: 10px;
   text-align: center;
-  margin-top: 2rem;
+  align-items: center;
+  background-color: #fee500;
+  box-shadow: 0;
+  cursor: pointer;
+  font-weight: 700;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.kakao-logo {
+  width: 30px;
+  margin: 5px;
 }
 </style>
